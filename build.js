82c50ffca434a82165f212edc5b5718324026b83
vishwas1 <vishwasanand.cse@gmail.com>
2025-05-26 11:21:29 +0530

    update

--- build.js
+++ build.js
@@ -3,11 +3,11 @@ const esbuild = require('esbuild');
 esbuild.build({
   entryPoints: ['web/index.tsx'],
   bundle: true,
-  outfile: 'android/app/src/main/assets/webview.js',
+  outfile: 'android/app/src/main/assets/ccd-crypto-sdk-for-react-native.js',
   target: ['es2020'],
   platform: 'browser',
   loader: {
-    
+
     '.ts': 'ts',
     '.tsx': 'tsx',
     '.wasm': 'base64', // Load wasm as Uint8Array (recommended)
